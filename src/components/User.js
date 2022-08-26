1ef86a0ba74b7893e26a5afa188a3556ef4969f7
Weronika Kuprel <ficekw@icloud.com>
2022-08-26 13:34:22 +0200

    change functional component for User to class component

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,17 @@
-import classes from './User.module.css';
+import React from "react";
+import classes from "./User.module.css";
+import { Component } from "react";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends Component {
+  //render method odpowiada returnowi
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+//zamieniam functional component na class component
+//const User = (props) => {
+//  return <li className={classes.user}>{props.name}</li>;
+//};
 
 export default User;
