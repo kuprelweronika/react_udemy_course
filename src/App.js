9e2cb24dbf5cabb19dc743491222d137fdf8ce64
Weronika Kuprel <ficekw@icloud.com>
2022-08-26 09:15:12 +0200

    add React memo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
+import Button from "./components/UI/Button/Button.js";
+import DemoOutput from "./components/UI/Button/Demo/DemoOutput";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph(!showParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
