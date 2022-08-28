f64ae30796043307f42c87ff1048019683ebe9d3
Weronika Kuprel <ficekw@icloud.com>
2022-08-28 11:14:41 +0200

    adding fetch with then() function

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  function fetchMoviesHandler() {
+    //przechwytuje dane z serwera
+    //wszystko wykonuje z funkcją .then() zeby miec gwarancje ze poprzednia rzecz sie wykonała zanim przejdzie dalej
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        return response.json(); //sciagam dane z json, to jest najpopularniejszy format danych
+        //transformuje go do js
+        //te dane pobrane muszą być przechowywane w stanie, który będzie updateowany i uzywany do MoviesList
+        //dodalam stan
+        //teraz array z bazy danych będzie nowym stanem
+        //przepisuje go na koniec funkcji z transformedMovies
+        //setMovies(data.results);
+
+        //moja aplikacja w komponencie Movies pobiera title, releaseDate i openingText, to sie nie zgadza z baza danych wiec trzeba to zmienic
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
