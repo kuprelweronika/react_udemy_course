4c22aed6524af1152090e40baf7b30edf08f710a
Weronika Kuprel <ficekw@icloud.com>
2022-08-30 17:45:19 +0200

    adding input validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,6 +1,10 @@
 import { useRef, useState } from "react";
 
+//mozna tez ustawic ref na inpucie i czytac wartosc tego refa
+//kiedy potrzebujemy
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef("");
   const [enteredName, setEnteredName] = useState("");
 
   const nameInputChangeHandler = (event) => {
@@ -12,12 +16,19 @@ const SimpleInput = (props) => {
     event.preventDefault();
     //musimy dac zeby nie wysylalo HTTP requesta
     console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
   };
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" onChange={nameInputChangeHandler} />
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
