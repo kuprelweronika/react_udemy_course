bc806b134413fbc36a15bf74efacd7a558237cb5
Weronika Kuprel <ficekw@icloud.com>
2022-08-30 17:30:09 +0200

    adding handlers

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,23 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+    //event dla kazdej wpisanej litery
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    //musimy dac zeby nie wysylalo HTTP requesta
+    console.log(enteredName);
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
