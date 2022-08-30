51b075e4605896ced98ce480009437cdeefbee7d
Weronika Kuprel <ficekw@icloud.com>
2022-08-30 18:03:20 +0200

    adding styles to validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -6,6 +6,8 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef("");
   const [enteredName, setEnteredName] = useState("");
+  //state dla walidacji
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -15,20 +17,37 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = (event) => {
     event.preventDefault();
     //musimy dac zeby nie wysylalo HTTP requesta
+
+    //robimy if zeby user nie mogl wrzucic empty form
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
     console.log(enteredName);
+    //to samo ale z uzyciem Ref, to nie jest zbyt dobre
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
   };
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
+  //wyzej jest warunek do zmiany klasy wzgledem tego czy input jest valid czy nie
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
+          value={enteredName}
         />
+        {!enteredNameIsValid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
