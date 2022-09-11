f5d887dcc0c6ab4166390bdf26f808218dabe936
Weronika Kuprel <ficekw@icloud.com>
2022-09-11 12:31:47 +0200

    added redux store data

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,28 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  //tutaj będą zawsze przechowywane najbardziej aktualne stany
+
+  const dispatch = useDispatch();
+  const incerementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incerementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decerement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
